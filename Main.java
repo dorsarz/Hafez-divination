31e77c936918a5ce5328dc3428cc782089a2080a
dorsa rezaei <dorsarezaei24@gmail.com>
2024-06-04 16:44:36 +0330

    Add files via upload
    main part

---
+++ Main.java
@@ -0,0 +1,8 @@
+// I got help from "if and else statement" YouTube channel
+
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException {
+     FaalGUI faalGUI=new FaalGUI();
+    }
+}
